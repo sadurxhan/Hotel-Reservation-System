2072ce525996b06bcf0ec8aed27a206d1450d1a7
Sadurxhan <it25102967@my.sliit.lk>
2026-09-26 18:24:18 +0530

    Add Payment entity

---
+++ src/main/java/com/loft/hotel/model/Payment.java
@@ -0,0 +1,74 @@
+package com.loft.hotel.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payment")
+public class Payment {
+    @Id
+    @Column(name = "payment_id")
+    private String paymentId;
+
+    @Column(name = "reservation_id", nullable = false)
+    private int reservationId;
+
+    @Column(name = "amount", precision = 10, scale = 2, nullable = false)
+    private BigDecimal amount;
+
+    @CreationTimestamp
+    @Column(name = "payment_datetime", nullable = false)
+    private LocalDateTime paymentDateTime;
+
+    public enum PaymentMethod {
+        Credit_Card, Debit_Card, Online_Banking, QR_Payment
+    }
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "payment_method", nullable = false)
+    private PaymentMethod paymentMethod;
+
+    public enum PaymentStatus{
+        Pending, Paid, Failed
+    }
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "payment_status", nullable = false)
+    private PaymentStatus paymentStatus;
+
+    @Column(name = "transaction_id", unique = true)
+    private String transactionId;
+
+    public Payment(){
+
+    }
+
+    public Payment(String paymentId, int reservationId, BigDecimal amount, PaymentMethod paymentMethod, PaymentStatus paymentStatus, String transactionId){
+        this.paymentId = paymentId;
+        this.reservationId = reservationId;
+        this.amount = amount;
+        this.paymentMethod = paymentMethod;
+        this.paymentStatus = paymentStatus;
+        this.transactionId = transactionId;
+    }
+    public void setPaymentId(String paymentId){ this.paymentId = paymentId; }
+    public String getPaymentId(){ return paymentId; }
+
+    public void setReservationId(int reservationId){ this.reservationId = reservationId; }
+    public int getReservationId(){ return reservationId; }
+
+    public void setAmount(BigDecimal amount){ this.amount = amount; }
+    public BigDecimal getAmount(){ return amount; }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod){ this.paymentMethod = paymentMethod; }
+    public PaymentMethod getPaymentMethod(){ return paymentMethod; }
+
+    public void setPaymentStatus(PaymentStatus paymentStatus){ this.paymentStatus = paymentStatus; }
+    public PaymentStatus getPaymentStatus(){ return paymentStatus; }
+
+    public void setTransactionId(String transactionId){ this.transactionId = transactionId; }
+    public String getTransactionId(){ return transactionId; }
+}
