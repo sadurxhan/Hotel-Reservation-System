dddf09cb80a1f235eb5f0e3bef46d9d01b090ebf
Sadurxhan <it25102967@my.sliit.lk>
2026-09-26 19:26:51 +0530

    Add Invoice entity

--- src/main/java/com/loft/hotel/model/Invoice.java
+++ src/main/java/com/loft/hotel/model/Invoice.java
@@ -1,4 +1,60 @@
 package com.loft.hotel.model;
 
-public class invoice {
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "invoice")
+public class Invoice {
+    @Id
+    @Column(name = "invoice_id", nullable = false)
+    private String invoiceId;
+
+    @Column(name = "payment_id", nullable = false, unique = true)
+    private String paymentId;
+
+    @Column(name = "total_amount", precision = 10, scale = 2, nullable = false)
+    private BigDecimal totalAmount;
+
+    public enum InvoiceStatus{
+        ISSUED, SENT
+    }
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "invoice_status", nullable = false)
+    private InvoiceStatus invoiceStatus;
+
+    @CreationTimestamp
+    @Column(name = "issued_date", nullable = false)
+    private LocalDateTime issuedDate;
+
+    // No-argument constructor
+    public Invoice(){
+    }
+
+    // Parameterized constructor
+    public Invoice( String invoiceId, String paymentId, BigDecimal totalAmount, InvoiceStatus invoiceStatus){
+        this.invoiceId = invoiceId;
+        this.paymentId = paymentId;
+        this.totalAmount = totalAmount;
+        this.invoiceStatus = invoiceStatus;
+    }
+
+    public void setInvoiceId(String invoiceId){ this.invoiceId = invoiceId; }
+    public String getInvoiceId(){ return invoiceId; }
+
+    public void setPaymentId(String paymentId){ this.paymentId = paymentId; }
+    public String getPaymentId(){ return paymentId; }
+
+    public void setTotalAmount(BigDecimal totalAmount){ this.totalAmount = totalAmount; }
+    public BigDecimal getTotalAmount(){ return totalAmount; }
+
+    public void setInvoiceStatus(InvoiceStatus invoiceStatus){ this.invoiceStatus = invoiceStatus; }
+    public InvoiceStatus getInvoiceStatus(){ return invoiceStatus; }
+
+    public LocalDateTime getIssuedDate(){ return issuedDate; }
+    public void setIssuedDate(LocalDateTime issuedDate){ this.issuedDate = issuedDate; }
 }
